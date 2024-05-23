e610afee981bae0acee7d7303ee370f66c88b359
Kamil Marszalek <kamilmar553@gmail.com>
2024-05-23 11:54:54 +0200

    changed main

--- juliaSet.s
+++ juliaSet.s
@@ -2,7 +2,7 @@ section .text
 global juliaSet
 
 juliaSet:
-    ; Arguments: 
+    ; Arguments:
     ; rdi = buffer address
     ; rsi = width
     ; rdx = height
@@ -38,77 +38,79 @@ innerLoop:
     divsd xmm6, xmm8 ; xmm6 = 2 * (col - offsetX) * escapeRadius / (width * scale)
 
     ; calc imag part xmm7 = zImag
-    cvtsi2sd xmm7, r8 ; xmm8 = row
-    subsd xmm7, xmm4 ; xmm8 = row - offsetY
-    mulsd xmm7, xmm0 ; xmm8 = (row - offsetY) * escapeRadius
-    addsd xmm7, xmm7 ; xmm8 = 2 * (row - offsetY) * escapeRadius
-    cvtsi2sd xmm8, rdx ; xmm7 = height
-    mulsd xmm8, xmm5 ; xmm7 = height * scale
-    divsd xmm7, xmm8 ; xmm8 = 2 * (row - offsetY) * escapeRadius / (height * scale)
+    cvtsi2sd xmm7, r8 ; xmm7 = row
+    subsd xmm7, xmm4 ; xmm7 = row - offsetY
+    mulsd xmm7, xmm0 ; xmm7 = (row - offsetY) * escapeRadius
+    addsd xmm7, xmm7 ; xmm7 = 2 * (row - offsetY) * escapeRadius
+    cvtsi2sd xmm8, rdx ; xmm8 = height
+    mulsd xmm8, xmm5 ; xmm8 = height * scale
+    divsd xmm7, xmm8 ; xmm7 = 2 * (row - offsetY) * escapeRadius / (height * scale)
 
     ; set iter counter
     mov rax, 0
 pixelLoop:
-    ; check if iter counter is zero
+    ; check if iter counter is 128
     cmp rax, 128
     je endPixelLoop
 
     ; calc zReal^2
-    movsd xmm8, xmm6 ; xmm7 = zReal
-    mulsd xmm8, xmm8 ; xmm7 = zReal^2
+    movsd xmm8, xmm6 ; xmm8 = zReal
+    mulsd xmm8, xmm6 ; xmm8 = zReal^2
     
     ; calc zImag^2
     movsd xmm9, xmm7 ; xmm9 = zImag
     mulsd xmm9, xmm7 ; xmm9 = zImag^2
 
-    ; xmm7 = zReal^2 + zImag^2
+    ; xmm8 = zReal^2 + zImag^2
     addsd xmm8, xmm9
 
     ; xmm9 = escapeRadius^2
     movsd xmm9, xmm0
     mulsd xmm9, xmm0
 
-    ; if zReal^2 + zImag^2 > escapeRadius^2, then break
+    ; if zReal^2 + zImag^2 >= escapeRadius^2, then break
     comisd xmm8, xmm9
     jge endPixelLoop
 
-    ; xmm7 = zReal^2 - zImag^2
-    movsd xmm8, xmm6 ; xmm7 = zReal
-    mulsd xmm8, xmm6 ; xmm7 = zReal^2
+    ; calc new zReal
+    movsd xmm8, xmm6 ; xmm8 = zReal
+    mulsd xmm8, xmm6 ; xmm8 = zReal^2
     movsd xmm9, xmm7 ; xmm9 = zImag
     mulsd xmm9, xmm9 ; xmm9 = zImag^2
-    subsd xmm8, xmm9 ; xmm7 = zReal^2 - zImag^2
+    subsd xmm8, xmm9 ; xmm8 = zReal^2 - zImag^2
+    addsd xmm8, xmm1 ; xmm8 = zReal^2 - zImag^2 + cReal
 
-    ; xmm10 = 2 * zReal * zImag + cImag
-    addsd xmm7, xmm7
-    mulsd xmm7, xmm6
-    addsd xmm7, xmm2
+    ; calc new zImag
+    movsd xmm9, xmm6 ; xmm9 = zReal
+    mulsd xmm9, xmm7 ; xmm9 = zReal * zImag
+    addsd xmm9, xmm9 ; xmm9 = 2 * zReal * zImag
+    addsd xmm9, xmm2 ; xmm9 = 2 * zReal * zImag + cImag
 
-    ; xmm11 = xmm7 + cReal
-    movsd xmm6, xmm8 ; xmm11 = zReal^2 - zImag^2
-    addsd xmm6, xmm1 ; xmm11 = zReal^2 - zImag^2 + cReal
+    ; update zReal and zImag
+    movsd xmm6, xmm8 ; zReal = xmm8
+    movsd xmm7, xmm9 ; zImag = xmm9
 
-    ; decrement iter counter
+    ; increment iter counter
     inc rax
     jmp pixelLoop
 endPixelLoop:
-   	; rcx = uint8_t r = ((maxIteration - iteration) * 255) / maxIteration;
-	mov		rcx, 128		; rcx = maxIteration
-	sub		rcx, rax		; rcx = maxIteration - iteration
-	imul	rcx, 255		; rcx = (maxIteration - iteration) * 255
-	sar		rcx, 7			; rcx = (maxIteration - iteration) * 255 / maxIteration (maxIteration == 128 == 2^7)
+    ; rcx = uint8_t r = ((maxIteration - iteration) * 255) / maxIteration
+    mov rcx, 128 ; rcx = maxIteration
+    sub rcx, rax ; rcx = maxIteration - iteration
+    imul rcx, 255 ; rcx = (maxIteration - iteration) * 255
+    sar rcx, 7 ; rcx = (maxIteration - iteration) * 255 / maxIteration (maxIteration == 128 == 2^7)
 
-	; rbx = pixelIdx = 3 * (row * width + col)
-	mov		rbx, r8			; rbx = row
-	imul	rbx, rsi		; rbx = row * width
-	add		rbx, r9			; rbx = row * width + col
-	mov		r12, rbx		; r12 = 1 * (row * width + col)
-	shl		rbx, 1			; rbx = 2 * (row * width + col)
-	add		rbx, r12		; rbx = 3 * (row * width + col)
+    ; rbx = pixelIdx = 3 * (row * width + col)
+    mov rbx, r8 ; rbx = row
+    imul rbx, rsi ; rbx = row * width
+    add rbx, r9 ; rbx = row * width + col
+    mov r12, rbx ; r12 = 1 * (row * width + col)
+    shl rbx, 1 ; rbx = 2 * (row * width + col)
+    add rbx, r12 ; rbx = 3 * (row * width + col)
 
-	mov 	[rdi + rbx], cl				; *(pixels + pixelIdx) = r
-	mov		byte [rdi + rbx + 1], 0		; *(pixels + pixelIdx + 1) = 0
-	mov		byte [rdi + rbx + 2], 0		; *(pixels + pixelIdx + 2) = 0
+    mov [rdi + rbx], cl ; *(pixels + pixelIdx) = r
+    mov byte [rdi + rbx + 1], 0 ; *(pixels + pixelIdx + 1) = 0
+    mov byte [rdi + rbx + 2], 0 ; *(pixels + pixelIdx + 2) = 0
 
     ; decrement col
     dec r9
